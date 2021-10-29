33dd5ed7ba31afd6df130818ed93c758b7477d0b
Alan Nonato da Silva <alan_nonato.silva@hotmail.com>
2021-10-28 23:36:36 -0300

    Adicionado models, migrations e seeders para parametrização da base de dados

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,9 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('telephone');
+            $table->boolean('situation');
+            $table->unsignedSmallInteger('type');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
