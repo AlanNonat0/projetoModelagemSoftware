9bd92e2fde4c703b7951c233636375dcf86447a7
Alan Nonato da Silva <alan_nonato.silva@hotmail.com>
2021-11-04 23:19:23 -0300

    fix https mix content

--- resources/views/layouts/_partials/toast_errors.blade.php
+++ resources/views/layouts/_partials/toast_errors.blade.php
@@ -3,7 +3,7 @@
 <div class="position-fixed top-0 end-0 p-3" style="z-index: 11">
 <div id="toastErrors" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
     <div class="toast-header text-white bg-danger">
-      <img src="{{ asset('img/exclam-white.png') }}" class alt="error" class="rounded me-2 bg-danger" width="15" height="14">
+      <img src="{{ asset('img/exclam-white.png', true) }}" class alt="error" class="rounded me-2 bg-danger" width="15" height="14">
       <strong class="me-auto" id="error-title" >Erro!</strong>
       <small id="error-status"></small>
       <button type="button" class="btn-close btn-close-white" data-bs-dismiss="toast" aria-label="Close"></button>
