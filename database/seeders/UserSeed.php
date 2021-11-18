fa0628d0c91363c7968761525bb3f5280ee86236
Alan Nonato da Silva <alan_nonato.silva@hotmail.com>
2021-11-18 10:28:02 -0300

    fix seeds

--- database/seeders/UserSeed.php
+++ database/seeders/UserSeed.php
@@ -6,7 +6,7 @@ use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
-class UserSeeder extends Seeder
+class UserSeed extends Seeder
 {
     /**
      * Run the database seeds.
